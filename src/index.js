1bacfc5d410655a9a55e0e4be3f12c68c4aa3867
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2019-09-21 21:20:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const WORD_LENGTH = 10;
+
+const to_morse = (word) => {
+    let morse_word = "";
+    for (let i = 0; i < WORD_LENGTH; i+=2){
+        let sym = '' + word[i] + word[i + 1];
+        switch(sym) {
+            case '10':
+                morse_word += ('.');
+                break;
+            case '11':
+                morse_word += ('-');
+                break;
+            case '00':
+                break;
+            case '**':
+                return " ";
+        }
+    }
+    return morse_word;
+};
+
+const to_letter = (morse_letter) => {
+    if (morse_letter === ' ') return ' ';
+    return MORSE_TABLE[morse_letter];    
+};
+
 function decode(expr) {
     // write your solution here
+    let arr = expr.split('');
+    let words = [];
+    let words_count = arr.length/WORD_LENGTH;
+    
+    for (let i = 0; i < words_count; i++){
+        words.push(arr.splice(0,WORD_LENGTH));
+    }
+    let morse_letters = words.map(to_morse);
+    let letters = morse_letters.map(to_letter);
+    let res_string = "";
+    for (let i in letters){
+        res_string += letters[i];
+    }
+    return res_string;
 }
 
 module.exports = {
